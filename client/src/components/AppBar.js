c634396278d9b883226e2e8445286259f3d615ce
George Bockari <bitwhys@gmail.com>
2020-04-25 18:34:15 -0700

    refactor(AppBar): add conditional to display back arrow if on create page

--- client/src/components/AppBar.js
+++ client/src/components/AppBar.js
@@ -1,37 +1,71 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 
-const AppBar = ({ route }) => {
+const BackArrow = () => (
+  <span
+    id="add-user"
+    className="cursor-pointer text-4xl
+     icon icon-cheveron-left text-gray-300
+     hover:text-teal-500 transition duration-150"
+  />
+)
+
+const AddNewContact = () => (
+  <span
+    id="add-user"
+    className="cursor-pointer text-4xl
+     icon icon-user-plus text-gray-300
+     hover:text-teal-500 transition duration-150"
+  />
+)
+
+const SearchInput = () => (
+  <div className="flex items-center">
+    <label className="mr-1" htmlFor="search">
+      <span className="cursor-pointer text-4xl icon icon-search" />
+    </label>
+    <input
+      type="text"
+      className="px-2 py-1"
+      placeholder="Search contacts"
+      name="search"
+      id="search"
+    />
+  </div>
+)
+
+const AppBar = ({ location }) => {
+  const { pathname } = location
   return (
     <div
       id="app-bar"
       className="bg-white flex justify-between items-center px-8 mb-4"
     >
-      <div className="flex items-center">
-        <label className="mr-1" htmlFor="search">
-          <span className="cursor-pointer text-4xl icon icon-search" />
-        </label>
-        <input
-          type="text"
-          className="px-2 py-1"
-          placeholder="Search contacts"
-          name="search"
-          id="search"
-        />
-      </div>
-      <Link to="/create" className="focus:outline-none">
-        <span
-          id="add-user"
-          className="cursor-pointer text-4xl
-           icon icon-user-plus text-gray-300
-           hover:text-teal-500 transition duration-150"
-        />
-      </Link>
+      {(pathname === '/' && (
+        <>
+          <SearchInput />
+          <Link to="/create" className="focus:outline-none">
+            <AddNewContact />
+          </Link>
+        </>
+      )) || (
+        <Link to="/">
+          <BackArrow />
+        </Link>
+      )}
     </div>
   )
 }
 
-AppBar.propTypes = {}
+AppBar.propTypes = {
+  location: PropTypes.shape({
+    hash: PropTypes.string,
+    key: PropTypes.string,
+    pathname: PropTypes.string,
+    search: PropTypes.string,
+    state: PropTypes.object,
+  }),
+}
 
-export default AppBar
+export default withRouter(AppBar)
